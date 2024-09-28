e196f7082c27a169b8a5bb9a9124d99bb44adda5
SahajIVVIX-1@users.noreply.github.com <SahajVIIX-1@users.noreply.github.com>
2024-09-29 00:51:36 +0530

    00:51 AM Commiting

--- rest api/Routes/Classes.route.js
+++ rest api/Routes/Classes.route.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.send("Getting List of All Classes...");
+// router.get("/", (req, res, next) => {
+//   res.send("Welcome to the ClassDashboard!");
+// });
+
+// router.post("/", (req, res, next) => {
+//   res.send("All classes will be Listed here!");
+// });
+
+router.get("/class", (req, res, next) => {
+  res.send("Printing Id ...");
+});
+
+router.patch("/:class", (req, res, next) => {
+  res.send("Patching Id ...");
+});
+
+router.delete("/:class", (req, res, next) => {
+  res.send("Deleting Id ...");
 });
 
 module.exports = router;
--- rest api/app.js
+++ rest api/app.js
@@ -1,15 +1,9 @@
 const express = require("express");
-
 const app = express();
 
-app.get("/", (req, res, next) => {
-  console.log(req.url, req.method);
-  res.send("Welcome to the Home Page!");
-});
-
-app.post("/", (req, res, next) => {});
-
-app.delete("/", (req, res, next) => {});
+//Calling ./rest api/Routes/Classes.route.js
+const ClassRoute = require("./Routes/Classes.route.js");
+app.use("/school", ClassRoute);
 
 app.listen(8000, () => {
   console.log("We are listening on Port:8000 ...");
