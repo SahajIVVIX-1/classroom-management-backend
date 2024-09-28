a7e69a163e5b6d35a0c63b9333b6a98834ea8682
Hil@3531 <142313407+HilKalathiya@users.noreply.github.com>
2024-09-28 21:28:26 +0530

    new schema

--- src/schema/assignment.js
+++ src/schema/assignment.js
@@ -4,6 +4,7 @@ const assignmentSchema = new mongoose.Schema({
   subname: String,
   assingmentName: String,
   dueDate: Date,
+  class_id: Number
 });
 
 const Assignment = mongoose.model("Assignment", assignmentSchema);
