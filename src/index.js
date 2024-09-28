4e7a7c581cfc46d1a94cce13f7b13bdaf830536a
SahajIVVIX-1@users.noreply.github.com <SahajVIIX-1@users.noreply.github.com>
2024-09-28 17:01:03 +0530

    final modal

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ const Assignment = require("./schema/assignment");
 const Notification = require("./schema/notification");
 const Student = require("./schema/student");
 const Attendence = require("./schema/attendence");
+const Classroom = require("./schema/classroom");
 
-mongoose.connect(
+mongoose
+  .connect(
     "mongodb+srv://23bit092:KN8Zena5bRfZ8npF@cluster0.ut8qk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => {
@@ -13,9 +15,3 @@ mongoose.connect(
   .catch((err) => {
     console.log(err);
   });
-
-  const student = new Student({name:"Hil Kalathiya",class:"1-A"})
-  student.save().then(()=>console.log("Student saved"))
-
-  const attendance = new Attendence({class_id:2,Attend_type:true,student_id:1,date: new Date})
-  attendance.save().then(()=>console.log("Attendance saved"))
